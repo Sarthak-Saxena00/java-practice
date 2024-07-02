469ae564eab96734140fa0576353a0f1dfe23aaa
Sarthak-Saxena00 <166148144+Sarthak-Saxena00@users.noreply.github.com>
2024-07-02 22:32:59 +0530

    02-07-24

---
+++ src/multithreading/IncrementCount.java
@@ -0,0 +1,52 @@
+package multithreading;
+
+public class IncrementCount {
+    private int count=1;
+    private final Object lk=new Object();
+
+    public void printOdd(){
+        synchronized (lk){
+            while (count<=10){
+                if(count%2!=0) {
+                    System.out.println(count + "<-------by thread 1");
+                    count++;
+                }
+                else {
+                    try{
+                        lk.wait();
+                    }
+                    catch (InterruptedException e){
+                        e.getStackTrace();
+                    }
+                }
+                lk.notify();
+            }
+        }
+    }public void printEven(){
+        synchronized (lk){
+            while (count<=10){
+                if(count%2==0) {
+                    System.out.println(count + "<--------by thread 2");
+                    count++;
+                }
+                else {
+                    try{
+                        lk.wait();
+                    }
+                    catch (InterruptedException e){
+                        e.getStackTrace();
+                    }
+                }
+                lk.notify();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        IncrementCount ic=new IncrementCount();
+        Thread t1=new Thread(ic::printOdd);
+        Thread t2=new Thread(ic::printEven);
+        t1.start();
+        t2.start();
+    }
+}
